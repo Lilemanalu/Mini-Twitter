947b841d0237dd9c56b5ba2790de4b3d51542adf
Lile Manalu <lilemanalu207@gmail.com>
2024-09-10 20:39:04 +0700

    Create UserRepository.java

---
+++ microservices/user-service/src/main/java/mini_twitter/user_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package mini_twitter.user_service.repository;
+
+import mini_twitter.user_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findFirstByToken(String token);
+}
