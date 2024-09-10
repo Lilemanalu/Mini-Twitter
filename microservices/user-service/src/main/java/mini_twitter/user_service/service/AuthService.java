5157d0385a6b754d1d695e68420918ff32a5e12e
Lile Manalu <lilemanalu207@gmail.com>
2024-09-10 22:11:53 +0700

    Implement user login API in AuthService.java

---
+++ microservices/user-service/src/main/java/mini_twitter/user_service/service/AuthService.java
@@ -0,0 +1,62 @@
+package mini_twitter.user_service.service;
+
+import jakarta.transaction.Transactional;
+import mini_twitter.user_service.dto.LoginUserRequest;
+import mini_twitter.user_service.dto.TokenResponse;
+import mini_twitter.user_service.entity.User;
+import mini_twitter.user_service.repository.UserRepository;
+import mini_twitter.user_service.security.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+public class AuthService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Transactional
+    public TokenResponse login(LoginUserRequest request) {
+        validationService.validate(request);
+
+        logger.info("Attempting to login user with username: {}", request.getUsername());
+
+        User user = userRepository.findById(request.getUsername())
+                .orElseThrow(() -> {
+                    logger.warn("Login failed for username: {} - User not found", request.getUsername());
+                    return new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or password wrong");
+                });
+
+        if (BCrypt.checkpw(request.getPassword(), user.getPassword())) {
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setTokenExpiredAt(next30Days());
+            userRepository.save(user);
+
+            logger.info("User {} logged in successfully. Token: {}", request.getUsername(), token);
+
+            return TokenResponse.builder()
+                    .token(token)
+                    .expiredAt(user.getTokenExpiredAt())
+                    .build();
+        } else {
+            logger.warn("Login failed for username: {} - Incorrect password", request.getUsername());
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username or password wrong");
+        }
+    }
+
+    private Long next30Days() {
+        return System.currentTimeMillis() + (1000 * 60 * 60 * 24 * 30);
+    }
+}
