5e479f9d5a0c07d31bad6fb12cba30cf5e090229
Lile Manalu <lilemanalu207@gmail.com>
2024-09-10 23:01:40 +0700

    Implement get user by ID endpoint in UserController.java

--- microservices/user-service/src/main/java/mini_twitter/user_service/controller/UserController.java
+++ microservices/user-service/src/main/java/mini_twitter/user_service/controller/UserController.java
@@ -1,15 +1,15 @@
 package mini_twitter.user_service.controller;
 
 import mini_twitter.user_service.dto.RegisterUserRequest;
+import mini_twitter.user_service.dto.UserResponse;
 import mini_twitter.user_service.dto.WebResponse;
 import mini_twitter.user_service.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class UserController {
@@ -33,6 +33,23 @@ public class UserController {
             return WebResponse.<String>builder().data("OK").build();
         } catch (Exception e) {
             logger.error("Error registering user: {}", request.getUsername(), e);
+            throw e;
+        }
+    }
+
+    @GetMapping(
+            path = "/api/users/{userId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> get(@PathVariable("userId") String userId) {
+        logger.debug("Received request to get user with ID: {}", userId);
+
+        try {
+            UserResponse userResponse = userService.get(userId);
+            logger.debug("Successfully retrieved user: {}", userResponse);
+            return WebResponse.<UserResponse>builder().data(userResponse).build();
+        } catch (ResponseStatusException e) {
+            logger.error("Error retrieving user with ID: {}. Error: {}", userId, e.getMessage());
             throw e;
         }
     }
