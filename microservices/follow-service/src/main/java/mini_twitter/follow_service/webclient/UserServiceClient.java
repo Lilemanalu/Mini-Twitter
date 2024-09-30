84e1cefd2afba9bfd0144adf1b41c67e9786a3a6
Lile Manalu <lilemanalu207@gmail.com>
2024-09-30 16:56:06 +0700

    Fix token handling and response parsing in UserServiceClient

--- microservices/follow-service/src/main/java/mini_twitter/follow_service/webclient/UserServiceClient.java
+++ microservices/follow-service/src/main/java/mini_twitter/follow_service/webclient/UserServiceClient.java
@@ -2,8 +2,15 @@ package mini_twitter.follow_service.webclient;
 
 import mini_twitter.follow_service.dto.ApiResponseWrapper;
 import mini_twitter.follow_service.dto.UserResponseDto;
+import mini_twitter.follow_service.dto.WebResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +27,22 @@ public class UserServiceClient {
         String url = "http://localhost:8081/api/users/me";
 
         try {
-            return restTemplate.getForObject(url, String.class);
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-API-TOKEN", token);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<WebResponseDto<String>> response = restTemplate.exchange(
+                    url, HttpMethod.GET, entity, new ParameterizedTypeReference<WebResponseDto<String>>() {});
+
+            return response.getBody().getData();
+        } catch (HttpClientErrorException e) {
+            logger.error("Error fetching user ID from token: {} - Status Code: {}, Response Body: {}",
+                    token, e.getStatusCode(), e.getResponseBodyAsString());
+            throw new RuntimeException("Failed to get user ID from token", e);
         } catch (Exception e) {
             logger.error("Error fetching user ID from token: {}", token, e);
-            throw new RuntimeException("Failed to get user ID from token");
+            throw new RuntimeException("Failed to get user ID from token", e);
         }
     }
 
