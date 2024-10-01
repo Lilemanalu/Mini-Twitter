a480efb0da989ecf7271df0e35fecca0d8991125
Lile Manalu <lilemanalu207@gmail.com>
2024-10-01 20:36:15 +0700

    Create CommentRepository.java

---
+++ microservices/comment-service/src/main/java/mini_twitter/comment_service/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package mini_twitter.comment_service.repository;
+
+import mini_twitter.comment_service.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, String> {
+}
