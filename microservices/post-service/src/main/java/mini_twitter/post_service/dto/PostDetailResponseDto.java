2dca7bf6622136487b5f0b18bf3c9b9ed344bd6d
Lile Manalu <lilemanalu207@gmail.com>
2024-10-02 15:51:08 +0700

    Update PostDetailResponseDto.java

--- microservices/post-service/src/main/java/mini_twitter/post_service/dto/PostDetailResponseDto.java
+++ microservices/post-service/src/main/java/mini_twitter/post_service/dto/PostDetailResponseDto.java
@@ -4,14 +4,23 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
 public class PostDetailResponseDto {
+
     private String id;
+
     private String userId;
+
     private String content;
+
     private LocalDateTime createdAt;
-//    private int likes;
+
+    private int likes;
+
+    private List<CommentResponseDto> comments;
+
 }
 
