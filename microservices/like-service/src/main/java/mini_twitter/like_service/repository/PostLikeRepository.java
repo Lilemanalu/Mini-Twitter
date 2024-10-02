891fbaa3036db81e72fffa5b640bdaf6a7d5077d
Lile Manalu <lilemanalu207@gmail.com>
2024-10-02 11:12:21 +0700

    Create PostLikeRepository

---
+++ microservices/like-service/src/main/java/mini_twitter/like_service/repository/PostLikeRepository.java
@@ -0,0 +1,10 @@
+package mini_twitter.like_service.repository;
+
+import mini_twitter.like_service.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikeRepository extends JpaRepository<PostLike, String> {
+    Optional<PostLike> findByPostIdAndUserId(String postId, String userId);
+}
